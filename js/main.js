f21ceeec3c411fc454c626cc086e9c9c0eb9719b
maxnovikovtga <mnovikov@tgadigital.com>
2018-09-04 21:05:58 +0300

    Add fade-in animation of elements, fix styles, fix layout

--- js/main.js
+++ js/main.js
@@ -1,26 +1,48 @@
-const body = document.querySelector('body'),
-      burgerBtn = document.getElementById('burger'),
-      navLink = document.querySelectorAll('.header__nav-link');
+window.onload = () => {
+    const body = document.querySelector('body'),
+        burgerBtn = document.getElementById('burger'),
+        navLink = document.querySelectorAll('.header__nav-link'),
+        fadeInElements = document.querySelectorAll('.animate__fade-in');
 
-for(let i = 0; i < navLink.length; i++) {
-    navLink[i].addEventListener("click", e => {
-        e.preventDefault();
-        var href = e.target.hash + '';
-        mobileClick();
+    function mobileClick() {
+        if (body.clientWidth < 768) {
+            burgerBtn.click();
+        }
+    }
 
-        document.querySelector(href).scrollIntoView({
-            behavior: 'smooth'
+    window.addEventListener('scroll', () => {
+
+        fadeInElements.forEach( (el, i) => {
+            const bottom_of_window = window.innerHeight,
+                bottom_of_object = el.getBoundingClientRect().top,
+                num = i%3 + 2;
+            el.style.transitionDelay = '.'+ num + 's';
+
+            if (parseInt(bottom_of_window) > bottom_of_object) {
+                el.style.opacity = '1';
+                el.style.transform = 'translateY(0)';
+            }
+            else {
+                el.style.opacity = '0';
+                el.style.transform = 'translateY(10px)';
+            }
         });
     });
-}
 
-burgerBtn.addEventListener("click", e => {
-    e.preventDefault();
-    body.classList.toggle('header_open');
-});
+    navLink.forEach((el) => {
+        el.addEventListener("click", e => {
+            e.preventDefault();
+            const href = e.target.hash + '';
+            mobileClick();
 
-function mobileClick() {
-    if (body.clientWidth < 768) {
-        burgerBtn.click();
-    }
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    burgerBtn.addEventListener("click", e => {
+        e.preventDefault();
+        body.classList.toggle('header_open');
+    });
 }
